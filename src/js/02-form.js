2ae3fbd16da7e5a6c73a4faf01aa319c2650a806
talamus1972 <talamus1972@gmail.com>
2023-12-17 13:46:20 +0200

    jjjjjj

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,1 +1,37 @@
-// https://github.com/Iryna-Vyshniak/goit-js-hw-07/blob/main/js/02-lightbox.js
+
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const input = form.querySelector("input");
+const textarea = form.querySelector("textarea");
+
+
+const initialMessage = localStorage.getItem(STORAGE_KEY);
+
+
+if (initialMessage) {
+    textarea.value = initialMessage;
+    input.value = initialMessage;
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (input === "") {
+        alert("Write email");
+    
+}
+    else if (textarea === "") {
+        alert("Write text");
+    }
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+})
+
+textarea.addEventListener("input", (event) => {
+    const message = event.currentTarget.value;
+    localStorage.setItem(STORAGE_KEY, message.trim());
+})
+
+input.addEventListener("input", (event) => {
+    const email = event.currentTarget.value;
+    localStorage.setItem(STORAGE_KEY, email.trim());
+})
